05c78b6b78dbec9af7cad9b3f05e973ad1e9a36e
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-09-21 12:20:59 +0000

    Deploying to gh-pages from @ Nadin2611/goit-js-hw-09@04bcf0720378b44c8a8e605d80009ff5a841b71d 🚀

---
+++ 01-color-switcher.23b4e699.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),bodyElement:document.body};let e;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,e=setInterval((function(){t.bodyElement.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.23b4e699.js.map
